fddc9bf26963ce7b8445afa7363e407028048849
ppd59006 <ppd59006@lad.pucrs.br>
2017-11-27 20:03:34 -0200

    -mudando nome do diretorio

--- run.sh
+++ run.sh
@@ -1,28 +1,36 @@
 echo -e "Percent 20"
-ladrun -np 1 frases -size 1000000 -percent 20
-ladrun -np 2 frases -size 1000000 -percent 20
-ladrun -np 4 frases -size 1000000 -percent 20
-ladrun -np 8 frases -size 1000000 -percent 20
-ladrun -np 16 frases -size 1000000 -percent 20
-ladrun -np 32 frases -size 1000000 -percent 20
+#ladrun -np 1 frases -size 1000000 -percent 20 result
+#ladrun -np 2 frases -size 1000000 -percent 20 result
+#ladrun -np 4 frases -size 1000000 -percent 20 result
+#ladrun -np 8 frases -size 1000000 -percent 20 result2
+#ladrun -np 16 frases -size 1000000 -percent 20 result2
+#ladrun -np 32 frases -size 1000000 -percent 20 result2
 echo -e "Percent 10"
-ladrun -np 1 frases -size 1000000 -percent 10
-ladrun -np 2 frases -size 1000000 -percent 10
-ladrun -np 4 frases -size 1000000 -percent 10
-ladrun -np 8 frases -size 1000000 -percent 10
-ladrun -np 16 frases -size 1000000 -percent 10
-ladrun -np 32 frases -size 1000000 -percent 10
+#ladrun -np 1 frases -size 1000000 -percent 10 result2
+#ladrun -np 2 frases -size 1000000 -percent 10 result3
+#ladrun -np 4 frases -size 1000000 -percent 10 result3
+#ladrun -np 8 frases -size 1000000 -percent 10 result3
+#ladrun -np 16 frases -size 1000000 -percent 10 result3
+#ladrun -np 32 frases -size 1000000 -percent 10 result4
 echo -e "Percent 30"
-ladrun -np 1 frases -size 1000000 -percent 30
-ladrun -np 2 frases -size 1000000 -percent 30
-ladrun -np 4 frases -size 1000000 -percent 30
-ladrun -np 8 frases -size 1000000 -percent 30
-ladrun -np 16 frases -size 1000000 -percent 30
-ladrun -np 32 frases -size 1000000 -percent 30
+#ladrun -np 1 frases -size 1000000 -percent 30
+#ladrun -np 2 frases -size 1000000 -percent 30 result5
+#ladrun -np 4 frases -size 1000000 -percent 30 result5
+#ladrun -np 8 frases -size 1000000 -percent 30 result5
+#ladrun -np 16 frases -size 1000000 -percent 30 result5
+#ladrun -np 32 frases -size 1000000 -percent 30 result5
 echo -e "Percent 40"
-ladrun -np 1 frases -size 1000000 -percent 40
-ladrun -np 2 frases -size 1000000 -percent 40
-ladrun -np 4 frases -size 1000000 -percent 40
-ladrun -np 8 frases -size 1000000 -percent 40
-ladrun -np 16 frases -size 1000000 -percent 40
-ladrun -np 32 frases -size 1000000 -percent 40
+#ladrun -np 1 frases -size 1000000 -percent 40
+#ladrun -np 2 frases -size 1000000 -percent 40 result5
+#ladrun -np 4 frases -size 1000000 -percent 40 result6
+#ladrun -np 8 frases -size 1000000 -percent 40 result6
+#ladrun -np 16 frases -size 1000000 -percent 40 result6
+#ladrun -np 32 frases -size 1000000 -percent 40 result6
+echo -e "Percent 50"
+ladrun -np 2 frases -size 1000000 -percent 50 
+ladrun -np 4 frases -size 1000000 -percent 50 
+ladrun -np 8 frases -size 1000000 -percent 50 
+ladrun -np 16 frases -size 1000000 -percent 50 
+ladrun -np 32 frases -size 1000000 -percent 50 
+
+
